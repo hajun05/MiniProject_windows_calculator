bac294baa1c65f8cb23e47620fb29b3ef5da6f84
Kim-yerin0904 <yeelin0904@naver.com>
2024-12-23 16:38:54 +0900

    단항연산, 소수점 기능 추가

---
+++ MiniProject_windows_calculator/AdditionalFunction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProject_windows_calculator
+{
+    internal class AdditionalFunction
+    {
+        //소수점 표시
+        public string DecimalPoint(string input)
+        {
+            if (!input.Contains('.'))
+            {
+                return input + '.';
+            }
+            else
+            {
+                return input;
+            }
+        }
+
+    }
+}
--- MiniProject_windows_calculator/MainForm.cs
+++ MiniProject_windows_calculator/MainForm.cs
@@ -15,7 +15,85 @@ namespace MiniProject_windows_calculator
         public Calculator()
         {
             InitializeComponent();
+            RHS_Output.Text = "0";
         }
 
+        // %버튼
+        private void Modulo_Click(object sender, EventArgs e)
+        {
+            UnaryOperations unaryOperations = new UnaryOperations();
+            //현재 값 검사 후 숫자가 있으면 가져와서 % 함수 실행
+            if (RHS_Output.Text != "" && RHS_Output.Text != "0으로 나눌 수 없습니다.")
+            {
+                string result = unaryOperations.Percent(RHS_Output.Text);
+                RHS_Output.Text = result;
+            }
+            //값이 없다면 아무 실행 X
+        }
+
+        //분수 버튼(1/x)
+        private void Fraction_Click(object sender, EventArgs e)
+        {
+            UnaryOperations unaryOperations = new UnaryOperations();
+            //현재 값 검사 후 숫자가 있으면 가져와서 분수 함수 실행
+            if (RHS_Output.Text != "" && RHS_Output.Text != "0으로 나눌 수 없습니다.")
+            {
+                string result = unaryOperations.Fraction(RHS_Output.Text);
+                RHS_Output.Text = result;
+            }
+            //값이 없다면 아무 실행 X
+        }
+
+        //제곱 버튼 x^2
+        private void SQR_Click(object sender, EventArgs e)
+        {
+            UnaryOperations unaryOperations = new UnaryOperations();
+            //현재 값 검사 후 숫자가 있으면 가져와서 제곱 함수 실행
+            if (RHS_Output.Text != "" && RHS_Output.Text != "0으로 나눌 수 없습니다.")
+            {
+                string result = unaryOperations.Square(RHS_Output.Text);
+                RHS_Output.Text = result;
+            }
+            //값이 없다면 아무 실행 X
+        }
+
+        //제곱근 버튼 
+        private void SquareRoot_Click(object sender, EventArgs e)
+        {
+            UnaryOperations unaryOperations = new UnaryOperations();
+            //현재 값 검사 후 숫자가 있으면 가져와서 제곱근 함수 실행
+            if (RHS_Output.Text != "" && RHS_Output.Text != "0으로 나눌 수 없습니다.")
+            {
+                string result = unaryOperations.Root(RHS_Output.Text);
+                RHS_Output.Text = result;
+            }
+            //값이 없다면 아무 실행 X
+        }
+
+        //소수점 버튼
+        private void DecimalPoint_Click(object sender, EventArgs e)
+        {
+            AdditionalFunction additionalFunction = new AdditionalFunction();
+            if (RHS_Output.Text != "" && RHS_Output.Text != "0으로 나눌 수 없습니다.")
+            {
+                string result = additionalFunction.DecimalPoint(RHS_Output.Text);
+                RHS_Output.Text = result;
+            }
+
+        }
+
+        //음수양수로 바꾸기
+        private void PlusMinus_Click(object sender, EventArgs e)
+        {
+            UnaryOperations unaryOperations = new UnaryOperations();
+            //현재 값 검사 후 숫자가 있으면 가져와서 음수양수 함수 실행
+            if (RHS_Output.Text != "" && RHS_Output.Text != "0으로 나눌 수 없습니다.")
+            {
+                string result = unaryOperations.NegativePositive(RHS_Output.Text);
+                RHS_Output.Text = result;
+            }
+            //값이 없다면 아무 실행 X
+        }
     }
 }
+
