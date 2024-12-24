a4f2bb5e9f1275ed0c8ab1cbb052c62badd2fb3c
hajun05 <hajun0325175@gmail.com>
2024-12-24 12:04:31 +0900

    미니프젝트 past assignment 기존 식이 존재할때, 존재하지 않을때 분기 구분

--- MiniProject_windows_calculator/ElementaryArithmetic.cs
+++ MiniProject_windows_calculator/ElementaryArithmetic.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MiniProject_windows_calculator
+﻿namespace MiniProject_windows_calculator
 {
     internal class ElementaryArithmetic
     {
         // 나누기 버튼
         public string division(string expression, string new_operand, char past_operator)
         {
-            
+
             if (past_operator == '=') // 이전에 이미 계산을 완료한 경우
             {
                 return (new_operand + " ÷ "); // 수식에 추가할 항 반환
@@ -26,7 +19,7 @@ namespace MiniProject_windows_calculator
         // 곱하기 버튼
         public string multiplication(string expression, string new_operand, char past_operator)
         {
-            
+
             if (past_operator == '=') // 이전에 이미 계산을 완료한 경우
             {
                 return (new_operand + " × "); // 수식에 추가할 항 반환
@@ -40,7 +33,7 @@ namespace MiniProject_windows_calculator
         // 빼기 버튼
         public string subtraction(string expression, string new_operand, char past_operator)
         {
-            
+
             if (past_operator == '=') // 이전에 이미 계산을 완료한 경우
             {
                 return (new_operand + " − "); // 수식에 추가할 항 반환
@@ -54,7 +47,7 @@ namespace MiniProject_windows_calculator
         // 더하기 버튼
         public string addition(string expression, string new_operand, char past_operator)
         {
-            
+
             if (past_operator == '=') // 이전에 이미 계산을 완료한 경우
             {
                 return (new_operand + " + "); // 수식에 추가할 항 반환
@@ -69,34 +62,43 @@ namespace MiniProject_windows_calculator
         public string past_assignment(string expression, string new_operand)
         {
             // 기존 식(좌항 연산자) 분리
-            string[] expression_terms = expression.Split(' ');
-
-            // 좌항 피연산자
-            double left_operand = double.Parse(expression_terms[0]);
-            // 연산자
-            string past_operator = expression_terms[1];
-            // 우항 피연산자
-            double right_operand = double.Parse(new_operand);
-
-            // 연산 결과
-            double result = 0.0;
-            switch (past_operator)
+            if (expression.Length > 0) // 기존 식이 존재할 때
             {
-                case "÷":
-                    result = left_operand / right_operand;
-                    break;
-                case "×":
-                    result = left_operand * right_operand;
-                    break;
-                case "−":
-                    result = left_operand - right_operand;
-                    break;
-                case "+":
-                    result = left_operand + right_operand;
-                    break;
+                string[] expression_terms = expression.Split(' ');
+
+                // 좌항 피연산자
+                double left_operand = double.Parse(expression_terms[0]);
+                // 연산자
+                string past_operator = expression_terms[1];
+                // 우항 피연산자
+                double right_operand = double.Parse(new_operand);
+
+                // 연산 결과
+                double result = 0.0;
+                switch (past_operator)
+                {
+                    case "÷":
+                        if (right_operand == 0)
+                            return "0으로 나눌 수 없습니다.";
+                        result = left_operand / right_operand;
+                        break;
+                    case "×":
+                        result = left_operand * right_operand;
+                        break;
+                    case "−":
+                        result = left_operand - right_operand;
+                        break;
+                    case "+":
+                        result = left_operand + right_operand;
+                        break;
+                }
+                return result.ToString();
+            }
+            else // 기존 식이 전무하면 새로 입력된 피연산자만 반환
+            {
+                return new_operand;
             }
 
-            return result.ToString();
         }
 
         // = 버튼
